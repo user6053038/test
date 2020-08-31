f6a5547ef7a18a7ca24980d06fae2ae802ef8d29
user6053038 <779357914@qq.com>
2020-08-31 14:48:54 +0800

    第一次提交

---
+++ src/cn/Demo01.java
@@ -0,0 +1,18 @@
+package cn;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Dennis
+ * @Date: 2020/07/28/21:42
+ * @Description:
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        int a = 10;
+		int b = 20;
+        System.out.println("Hello world!");
+        System.out.println("第一次提交");
+
+    }
+}
